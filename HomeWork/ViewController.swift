0bdbd10f84b1278d08f1bc2f167e41b84d84b9b2
Nastya <nastenushka98@mail.ru>
2023-02-27 19:46:24 +0300

    Initial Commit

---
+++ HomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork
+//
+//  Created by Анастасия Корнасевич on 27.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
